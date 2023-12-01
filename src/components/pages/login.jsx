b1bac3877ce2c53876c1266dae4f1d30e72b7f18
jvitor88 <80867252+jvitor88@users.noreply.github.com>
2023-12-01 19:39:09 -0300

    ajuste fav

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
 
     return (
         <div className='flex flex-col items-center justify-center gap-8 h-screen w-screen bg-mainbg'>
-            <form className='flex flex-col justify-center items-center gap-8 bg-white w-3/12 p-6 rounded-2xl' onSubmit={handleLogin}>
+            <form className='flex flex-col justify-center items-center gap-8 bg-white p-6 rounded-2xl m-5' onSubmit={handleLogin}>
                 <img src={logo} alt="Logo da Empresa" className='w-2/6' />
                 <div className='flex flex-col gap-4 w-full'>
                     <Label htmlFor="email1" value="E-mail" className='text-base font ml-3'/>
--- src/components/pages/suport.jsx
+++ src/components/pages/suport.jsx
@@ -12,9 +12,9 @@ const Suport = () => {
         <div className='flex flex-col items-center justify-around h-4/6 mt-5'>
           <h1 className='text-3xl font-bold'>Entre em contato conosco!</h1>
           <div className='flex gap-8'>
-            <div className='flex flex-col gap-6 items-center bg-white w-96 p-10 text-9xl rounded-xl cursor-pointer hover:scale-110'><FaHeadset /> <span className='text-3xl'>Telefone</span></div>
-            <div className='flex flex-col gap-6 items-center bg-white w-96 p-10 text-9xl rounded-xl cursor-pointer hover:scale-110'><FaComment /> <span className='text-3xl'>Chat</span></div>
-            <div className='flex flex-col gap-6 items-center bg-white w-96 p-10 text-9xl rounded-xl cursor-pointer hover:scale-110'><FaEnvelope /><span className='text-3xl'>E-mail</span></div>
+            <div className='flex flex-col gap-6 items-center bg-white w-80 p-10 text-9xl rounded-xl cursor-pointer hover:scale-110'><FaHeadset /> <span className='text-3xl'>Telefone</span></div>
+            <div className='flex flex-col gap-6 items-center bg-white w-80 p-10 text-9xl rounded-xl cursor-pointer hover:scale-110'><FaComment /> <span className='text-3xl'>Chat</span></div>
+            <div className='flex flex-col gap-6 items-center bg-white w-80 p-10 text-9xl rounded-xl cursor-pointer hover:scale-110'><FaEnvelope /><span className='text-3xl'>E-mail</span></div>
           </div>
         </div>
       </div>
